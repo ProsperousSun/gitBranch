888e6d32920623a62d25a28cc70d211054ea0d72
sunlirao <1104001740@qq.com>
2018-12-03 14:13:47 +0800

    将branchOne的branchOne类,merge到了branchTwo,添加一条,现在提交上去.

--- src/main/java/branchOne.java
+++ src/main/java/branchOne.java
@@ -1,5 +1,10 @@
 public class branchOne {
     public static void main(String[] args) {
         System.out.println("看看这次能不能merge到branchTwo!!!!");
+        method();
+    }
+
+    private static void method() {
+        System.out.println("这次能够merge成功吧!");
     }
 }
